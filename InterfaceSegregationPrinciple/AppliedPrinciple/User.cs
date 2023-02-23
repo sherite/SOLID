6f40e1bfd2fd94c2d66a6f3f63cf760b63a46962
sherite <sherite@hotmail.com>
2023-02-23 09:25:55 -0300

    SOLID I - Update

--- InterfaceSegregationPrinciple/AppliedPrinciple/User.cs
+++ InterfaceSegregationPrinciple/AppliedPrinciple/User.cs
@@ -1,12 +1,84 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AppliedPrinciple
 {
-    internal class User
+    
+        public interface IBasicActions<T>
+        {
+            T Get(int id);
+            List<T> GetList();
+            void Add(T entity);
+        }
+
+        public interface IEditableActions<T>
+        {
+            void Update(T entity);
+            void Delete(int id);
+        }
+
+        public interface ICRUD<T> : IBasicActions<T>, IEditableActions<T> { }
+
+        public class User
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Email { get; set; }
+        }
+
+    public class UserService : ICRUD<User>
     {
+        public void Add(User entity)
+        {
+            Console.WriteLine("Creation of user.");
+        }
+
+        public void Delete(int id)
+        {
+            Console.WriteLine("Delete user by id.");
+        }
+
+        public User Get(int id)
+        {
+            Console.WriteLine("Return user by id");
+            return new User();
+        }
+
+        public List<User> GetList()
+        {
+            Console.WriteLine("Get a list of users.");
+            return new List<User>();
+        }
+
+        public void Update(User entity)
+        {
+            Console.WriteLine("Edit a user.");
+        }
+    }
+
+    public class Sale
+    {
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+    }
+
+    public class SaleService : IBasicActions<Sale>
+    {
+        public void Add(Sale entity)
+        {
+            Console.WriteLine("Creation of Sale.");
+        }      
+
+        public Sale Get(int id)
+        {
+            Console.WriteLine("Return Sale by id");
+            return new Sale();
+        }
+
+        public List<Sale> GetList()
+        {
+            Console.WriteLine("Get a list of Sales.");
+            return new List<Sale>();
+        }
     }
 }
--- InterfaceSegregationPrinciple/StudyCase/User.cs
+++ InterfaceSegregationPrinciple/StudyCase/User.cs
@@ -1,12 +1,96 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace StudyCase
 {
-    internal class User
+    public interface ICRUD<T>
     {
+        T Get(int id);
+        List<T> GetList();
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(int id);
+    }
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+
+    public class UserService : ICRUD<User>
+    {
+        public void Add(User entity)
+        {
+           Console.WriteLine("Creation of user.");
+        }
+
+        public void Delete(int id)
+        {
+            Console.WriteLine("Delete user by id.");
+        }
+
+        public User Get(int id)
+        {
+            Console.WriteLine("Return user by id");
+            return new User();
+        }
+
+        public List<User> GetList()
+        {
+            Console.WriteLine("Get a list of users.");
+            return new List<User>();
+        }
+
+        public void Update(User entity)
+        {
+            Console.WriteLine("Edit a user.");
+        }
+    }
+
+    // ---------------------------
+
+    public class Sale
+    {
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+    }
+
+    public class SaleService : ICRUD<Sale>
+    {
+        public void Add(Sale entity)
+        {
+            Console.WriteLine("Creation of sale.");
+        }
+
+        public Sale Get(int id)
+        {
+            Console.WriteLine("Get a sale.");
+            return new Sale();
+        }
+
+        public List<Sale> GetList()
+        {
+            Console.WriteLine("Get list of sales.");
+            return new List<Sale>();    
+        }
+
+        // FR #1
+        // SALES CAN NOT UPDATE.
+        
+        // FR #2
+        // SALES CAN NOT DELETE.
+
+        public void Update(Sale entity)
+        {
+           // ### WARNING!!! SOLID I PRINCIPLE BROKEN
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            // ### WARNING!!! SOLID I PRINCIPLE BROKEN
+            throw new NotImplementedException();
+        }       
     }
 }
