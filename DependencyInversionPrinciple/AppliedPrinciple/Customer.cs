ddcf929bd1f5d9f7254d8e33fad4c00ab2af8a70
sherite <sherite@hotmail.com>
2023-02-23 11:59:54 -0300

    SOLID D - Update

---
+++ DependencyInversionPrinciple/AppliedPrinciple/Customer.cs
@@ -0,0 +1,28 @@
+﻿namespace AppliedPrinciple
+{
+   
+    public class Customer
+    {
+        public void Purchase(IPricing pricing)
+        {
+            decimal price = pricing.GetPrice();
+            // Code for make the purchase.
+        }
+    }
+
+    public interface IPricing
+    {
+        decimal GetPrice();
+
+    }
+
+    public class Product : IPricing
+    {
+        public decimal Price { get; set; }
+
+        public decimal GetPrice()
+        {
+            return Price;
+        }
+    }
+}
---
+++ DependencyInversionPrinciple/StudyCase/Customer.cs
@@ -0,0 +1,16 @@
+﻿namespace StudyCase
+{
+    public class Customer
+    {
+        public void Purchase(Product product)
+        {
+            // Code for make the purchase
+
+        }
+    }
+
+    public class Product
+    {
+        public decimal Price { get; set; }
+    }
+}
